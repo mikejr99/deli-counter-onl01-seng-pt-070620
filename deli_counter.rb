6672e9fa6a9e177bdac445528958fdc1aa2d1d01
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-18 23:35:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line (deli)
   if deli.length == 0
     puts "The line is currently empty."
   else
-    current_line = "the line is currently:"
+    current_line = "The line is currently:"
     deli.each.with_index(1) do |person, i|
       current_line << " #{i}. #{person}"
     end
