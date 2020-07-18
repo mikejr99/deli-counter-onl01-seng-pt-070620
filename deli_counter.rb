531c7ac861054dbb98084c1bb48f7a7d3b22bc67
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-18 23:50:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def now_serving(deli)
   if deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{deli.first}"
+    puts "Currently serving #{deli.first}."
     deli.shift
   end
 end
